8102c7ae34e3ac4137226113b5298e497d8aea95
anirut2543 <107028046+anirut2543@users.noreply.github.com>
2022-12-04 19:32:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var text = document.querySelector('.text');
+var percent = document.querySelector('.percent');
+var progress = document.querySelector('.progress');
+var count = 4;
+var per = 16;
+var loading = setInterval(animate, 50);
+let popup = document.getElementById("popup");
+
+    function openPopup(){
+        popup.classList.add("open-popup");
+    }
+    function closePopup(){
+        popup.classList.remove("open-popup");
+    }
+
+function animate(){
+  if(count == 100 && per == 400){
+    text.textContent = "Completed";
+    text.style.fontSize = "70px";
+    text.classList.add("add");
+    clearInterval(loading);
+    per = per + 4;
+    count = count + 1;
+    window.location='QR.html';
+  }else{
+    per = per + 4;
+    count = count + 1;
+    progress.style.width = per + 'px';
+    percent.textContent = count + '%';
+  }
+}
